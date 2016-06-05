4bc3f75c0a33041b20835d7ff68dbeb0a2ee4ef4
rhn <gihu.rhn@porcupinefactory.org>
2016-06-05 19:18:38 +0200

    Simplification works

--- src/smooth.py
+++ src/smooth.py
@@ -4,7 +4,7 @@ import gpxpy
 
 from collections import namedtuple
 import datetime
-from itertools import compress, tee
+from itertools import compress, tee, chain
 
 
 class FutureIter:
@@ -32,6 +32,7 @@ def n900_uncertainty_m(point):
 
 def timediff(pt1, pt2):
     return pt1.time - pt2.time
+
 
 def does_overlap(it, initial, get_uncertainty):
     def check_overlap(pt, other):
@@ -63,6 +64,20 @@ def is_moving(start, future, get_uncertainty):
     return None # not enough time points available
 
 
+def until(points, time):
+    for point in points:
+        if point.time > time:
+            return
+        yield point
+
+def split_time(points, difference):
+    points = FutureIter(points)
+    for start, future in points:
+        segment = [start] + list(until(future, start.time + difference))
+        points.advance_after(segment[-1])
+        yield segment
+
+
 def time_margins(it, margin):
     it = list(it)
     if len(it) == 0:
@@ -75,10 +90,32 @@ def time_margins(it, margin):
         return 0
 
     start = it[0]
-    start_idx = find_idx(it, lambda point: time_diff(start, point))
-    end_idx = len(it) - find_idx(reverse(it), lambda point: time_diff(point, start))
+    start_idx = find_idx(it, lambda point: timediff(start, point))
+    end_idx = len(it) - find_idx(reversed(it), lambda point: timediff(point, start))
     return it[start_idx:end_idx]
 
+class OnDemand:
+    def __init__(self, seq):
+        self.seq = seq
+        self.it = None
+        self.last_item = []
+
+    def __iter__(self):
+        self.it = seq.__iter__()
+        while True:
+            if len(self.last_item) == 0:
+                self.last_item.append(self.it.next())
+            yield self.last_item[-1]
+    
+    def next(self):
+        self.last_item = []
+
+def filter_out_segments(base, segments):
+    checker = OnDemand(chain(segments))
+    for point, p2 in zip(base, checker):
+        if point == p2:
+            checker.next()
+        yield point
 
 def find_stops(segment, get_uncertainty):
     """Good speed.
@@ -89,10 +126,9 @@ def find_stops(segment, get_uncertainty):
         stopped_points = list(while_overlap([point] + list(future), [], get_uncertainty))
         last = stopped_points[-1]
         if timediff(last, point) < TRIGGER_TIME:
-            #yield 'movement', stopped_points
-            pass
+            yield from (('move', point) for point in stopped_points)
         else:
-            yield stopped_points
+            yield 'pause', stopped_points
         seg_iter.advance_after(last)
 
 
@@ -103,27 +139,60 @@ def find_stops2(segment, get_uncertainty):
     seg_iter = FutureIter(segment)
     for point, future in seg_iter:
         if is_moving(point, future, get_uncertainty) is not False:
-            #yield 'move', point
+            yield 'move', point
             continue
         stopped_points = list(while_overlap([point] + list(future), [], get_uncertainty))
-        yield time_margins(stopped_points, TRIGGER_TIME / 2)
+        yield 'pause', stopped_points#time_margins(stopped_points, TRIGGER_TIME / 2)
         seg_iter.advance_after(stopped_points[-1])
 
+def weighted_average(items):
+    items = list(items)
+    total_weight = sum(item[1] for item in items)
+    return sum(item[0] * item[1] for item in items) / total_weight
 
-def replace_stops(segment, stops):
-    from pprint import pprint
-    for stop in stops:
-        pprint(stop)
+def time_average(times):
+    times = list(times)
+    return datetime.datetime.fromtimestamp(sum(int(time.timestamp()) for time in times) / len(times))
 
-def save_stops(stops):
+
+def find_centroid_simple(points, get_uncertainty):
+    points = list(points)
+    lat = weighted_average((point.latitude, 1 / get_uncertainty(point).horz) for point in points)
+    lon = weighted_average((point.longitude, 1 / get_uncertainty(point).horz) for point in points)
+    ele = weighted_average((point.elevation, 1 / get_uncertainty(point).vert) for point in points)
+    time = time_average(point.time for point in points)
+    return gpxpy.gpx.GPXTrackPoint(lat, lon, ele, time, name="stop")
+
+
+def simplify_stop(points, get_uncertainty):
+    for pts in split_time(points, datetime.timedelta(seconds=60)):
+        yield find_centroid_simple(pts, get_uncertainty)
+
+def replace_stops(stops_iter, get_uncertainty, skip_moves=False):
+    new_points = []
+    for action, data in stops_iter:
+        if action == 'move' and not skip_moves:
+            yield data
+        elif action == 'pause':
+            yield from simplify_stop(data, get_uncertainty)
+        else:
+            raise Exception("Invalid action")
+
+def pauses_only(stops):
+    return (stop[1] for stop in stops if stop[0] == 'pause')
+
+def save_simplified_stops(output, stops, get_uncertainty):
+    save_segments(output, (simplify_stop(seg, get_uncertainty) for seg in pauses_only(stops)))
+
+def save_segments(output, segments):
     gpx = gpxpy.gpx.GPX()
     track = gpxpy.gpx.GPXTrack()
     gpx.tracks.append(track)
-    for seg in stops:
+    for seg in segments:
         segment = gpxpy.gpx.GPXTrackSegment()
         track.segments.append(segment)
         segment.points.extend(seg)
-    print(gpx.to_xml())
+    print(gpx.to_xml(), file=output)
 
 stop_finders = {'good': find_stops2,
                 'fast': find_stops}
@@ -133,17 +202,20 @@ if __name__ == '__main__':
     import sys
     parser = argparse.ArgumentParser()
     parser.add_argument('file')
-    parser.add_argument('--method', choices=stop_finders, dafault='good')
-    parser.parse_args()
-    with open(parser.file) as infile:
+    parser.add_argument('--method', choices=stop_finders, default='good')
+    args = parser.parse_args()
+    with open(args.file) as infile:
         gpx = gpxpy.parse(infile)
     
     output = sys.stdout
     
-    stop_finder = stop_finders[parser.method]
+    stop_finder = stop_finders[args.method]
     
     get_uncertainty_m = n900_uncertainty_m
     for track in gpx.tracks:
         for segment in track.segments:
+            stops = stop_finder(segment.points, get_uncertainty_m)
             #save_stops(output, stop_finder(segment.points, get_uncertainty_m))
-            replace_stops(segment, stop_finder(segment.points, get_uncertainty_m))
+            save_simplified_stops(output, stops, get_uncertainty_m)
+            #segment.points = replace_stops(stop_finder(segment.points, get_uncertainty_m), get_uncertainty_m)
+#    print(gpx.to_xml(), file=output)
