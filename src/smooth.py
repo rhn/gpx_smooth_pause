ae8f27fc8a7caa1ee6588cb86c71076a41d772b2
rhn <gihu.rhn@porcupinefactory.org>
2016-06-05 13:50:59 +0200

    Added gpx smoother

---
+++ src/smooth.py
@@ -0,0 +1,149 @@
+#! /usr/bin/python3
+
+import gpxpy
+
+from collections import namedtuple
+import datetime
+from itertools import compress, tee
+
+
+class FutureIter:
+    def __init__(self, it):
+        self.it = list(it)
+        self.next_ptr = 0
+    
+    def __iter__(self):
+        for i, item in enumerate(self.it):
+            if self.next_ptr > i:
+                continue                
+            yield item, self.it[i + 1:]
+    
+    def advance_after(self, item):
+        self.next_ptr = self.it.index(item) + 1 # FIXME: accept indices
+
+TRIGGER_TIME = datetime.timedelta(seconds=10)
+
+
+def n900_uncertainty_m(point):
+    FACTOR = 10
+    horz = float(point.extensions['hdopCM']) / 100 / FACTOR
+    vert = float(point.extensions['vdopCM']) / 100 / FACTOR
+    return namedtuple("Uncertainty", ['horz', 'vert'])(horz, vert)
+
+def timediff(pt1, pt2):
+    return pt1.time - pt2.time
+
+def does_overlap(it, initial, get_uncertainty):
+    def check_overlap(pt, other):
+        return pt.distance_2d(other) < get_uncertainty(pt).horz + get_uncertainty(other).horz
+ 
+    checked = list(initial)
+    for point in it:
+        if not all(check_overlap(point, c) for c in checked):
+            break
+        checked.append(point)
+        yield True
+    yield False
+
+def while_overlap(it, initial, get_uncertainty):
+    it1, it2 = tee(it, 2)
+    for i, overlap in zip(it1, does_overlap(it2, initial, get_uncertainty)):
+        if not overlap:
+            break
+        yield i 
+
+
+def is_moving(start, future, get_uncertainty):
+    futures1, futures2 = tee(future, 2)
+    for end, overlaps in zip(futures1, does_overlap(futures2, [start], get_uncertainty)):
+        if not overlaps:
+            return True
+        if timediff(end, start) > TRIGGER_TIME:
+            return False
+    return None # not enough time points available
+
+
+def time_margins(it, margin):
+    it = list(it)
+    if len(it) == 0:
+        return
+
+    def find_idx(it, compare):
+        for i, point in enumerate(it):
+            if compare(point) > margin:
+                return i
+        return 0
+
+    start = it[0]
+    start_idx = find_idx(it, lambda point: time_diff(start, point))
+    end_idx = len(it) - find_idx(reverse(it), lambda point: time_diff(point, start))
+    return it[start_idx:end_idx]
+
+
+def find_stops(segment, get_uncertainty):
+    """Good speed.
+    Advances by TRIGGER_TIME or by overlapping points each time, whichever comes first.
+    """
+    seg_iter = FutureIter(segment)
+    for point, future in seg_iter:
+        stopped_points = list(while_overlap([point] + list(future), [], get_uncertainty))
+        last = stopped_points[-1]
+        if timediff(last, point) < TRIGGER_TIME:
+            #yield 'movement', stopped_points
+            pass
+        else:
+            yield stopped_points
+        seg_iter.advance_after(last)
+
+
+def find_stops2(segment, get_uncertainty):
+    """Good sensitivity.
+    Advances by overlapping points in paused mode, and by single point in moving mode. Checks for movement for each point, catching stops early and approximately in the middle.
+    """
+    seg_iter = FutureIter(segment)
+    for point, future in seg_iter:
+        if is_moving(point, future, get_uncertainty) is not False:
+            #yield 'move', point
+            continue
+        stopped_points = list(while_overlap([point] + list(future), [], get_uncertainty))
+        yield time_margins(stopped_points, TRIGGER_TIME / 2)
+        seg_iter.advance_after(stopped_points[-1])
+
+
+def replace_stops(segment, stops):
+    from pprint import pprint
+    for stop in stops:
+        pprint(stop)
+
+def save_stops(stops):
+    gpx = gpxpy.gpx.GPX()
+    track = gpxpy.gpx.GPXTrack()
+    gpx.tracks.append(track)
+    for seg in stops:
+        segment = gpxpy.gpx.GPXTrackSegment()
+        track.segments.append(segment)
+        segment.points.extend(seg)
+    print(gpx.to_xml())
+
+stop_finders = {'good': find_stops2,
+                'fast': find_stops}
+
+if __name__ == '__main__':
+    import argparse
+    import sys
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file')
+    parser.add_argument('--method', choices=stop_finders, dafault='good')
+    parser.parse_args()
+    with open(parser.file) as infile:
+        gpx = gpxpy.parse(infile)
+    
+    output = sys.stdout
+    
+    stop_finder = stop_finders[parser.method]
+    
+    get_uncertainty_m = n900_uncertainty_m
+    for track in gpx.tracks:
+        for segment in track.segments:
+            #save_stops(output, stop_finder(segment.points, get_uncertainty_m))
+            replace_stops(segment, stop_finder(segment.points, get_uncertainty_m))
